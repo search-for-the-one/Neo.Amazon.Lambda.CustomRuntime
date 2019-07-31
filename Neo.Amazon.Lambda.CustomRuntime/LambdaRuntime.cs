ebdb1ba02a6973467388a63848ae20af502b5059
Zach Saw <zach.saw@carsales.com.au>
2019-07-31 17:22:40 +1000

    Make LambdaRuntime public

--- Neo.Amazon.Lambda.CustomRuntime/LambdaRuntime.cs
+++ Neo.Amazon.Lambda.CustomRuntime/LambdaRuntime.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Neo.Amazon.Lambda.CustomRuntime
 {
-    internal static class LambdaRuntime
+    public static class LambdaRuntime
     {
         public static bool IsRunning => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("AWS_LAMBDA_RUNTIME_API"));
     }
